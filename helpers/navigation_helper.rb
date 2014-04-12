47cca195977c3380827e58888075aa34a0bff4b6
Roman Sklenář <mail@romansklenar.cz>
2014-04-12 20:53:18 +0200

    Implemented NavigationHelper

---
+++ helpers/navigation_helper.rb
@@ -0,0 +1,17 @@
+module NavigationHelper
+  def nav_link(name = nil, options = nil, condition = nil, html_options = nil, &block)
+    html_options, condition, options = condition, options, name if block_given?
+    options ||= {}
+
+    path = options.start_with?('/') ? options[1..-1] : options
+    if condition || (condition.nil? && current_page.path == path)
+      html_options ||= { }
+      html_options[:class] = '' unless html_options[:class]
+      html_options[:class] << ' active'
+    end
+
+    content_tag(:li, html_options) do
+      link_to(name, options, html_options, &block)
+    end
+  end
+end
